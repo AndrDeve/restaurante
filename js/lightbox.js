a8f8d426711de841776d004908b001ecbedfd71a
AndyR <marlon_rivera95@hotmail.com>
2022-06-16 11:13:02 -0500

    primera222

--- js/lightbox.js
+++ js/lightbox.js
@@ -21,3 +21,28 @@ const aparecerImagen = (imagen)=>{
     contenedorLight.classList.toggle('show')
     imagenLight.classList.toggle('showImage')
 }
+
+
+///////////////////////////////////////////////////////////////////
+const imagenes2 = document.querySelectorAll('.img-welcome2')
+const imagenLight2 = document.querySelector('.agregar-imagen2');
+const contenedorLight2 = document.querySelector('.imagen-light2')
+
+imagenes2.forEach(imagen2 => {
+    imagen2.addEventListener('click',()=>{
+        aparecerImagen2(imagen2.getAttribute('src'));
+    })
+});
+
+contenedorLight2.addEventListener('click',(e)=>{
+    if(e.target !== imagenLight2){
+        contenedorLight2.classList.toggle('show2')
+        imagenLight2.classList.toggle('showImage2')
+    }
+})
+
+const aparecerImagen2 = (imagen2)=>{
+    imagenLight2.src = imagen2;
+    contenedorLight2.classList.toggle('show2')
+    imagenLight2.classList.toggle('showImage2')
+}
