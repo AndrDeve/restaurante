bb5a2593d622f111db3720ed23046b9110e00925
AndyR <marlon_rivera95@hotmail.com>
2022-06-22 12:32:40 -0500

    resta111

---
+++ js/scroll.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.scroll-up' , {
+        duration: 1800,
+        origin: 'bottom',
+        distance: '50px'
+    });
+
+    window.sr = ScrollReveal();
+
+    sr.reveal('.scroll-right' , {
+        duration: 1800,
+        origin: 'right',
+        distance: '20px'
+    });
+
+    window.sr = ScrollReveal();
+
+    sr.reveal('.scroll-left' , {
+        duration: 1800,
+        origin: 'left',
+        distance: '20px'
+    });
+
+    // window.sr = ScrollReveal();
+
+    // sr.reveal('.scroll3' , {
+    //     duration: 1800,
+    //     origin: 'bottom',
+    //     distance: '50px'
+    // });
